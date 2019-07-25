8be38ef546a6a2a23938a46e1d095e663e36524a
Jamie Westell <jwestell@gmail.com>
2019-07-24 21:07:42 -0700

    Updated to use scm attribute

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,20 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
 
 class HelloConan(ConanFile):
     name = "hello"
     version = "0.1"
     author = "Jamie Westell jwestell@gmail.com"
-    url = "https://github.com/jwestell/hello"
+    url = ""
     license = "license"
     description = "Hello, World!"
     settings = "os", "compiler", "build_type", "arch"
-
-    def source(self):
-        self.run("git clone https://github.com/jwestell/hello.git")
-        self.run("cd hello && git checkout master")
+    scm = {
+        "type": "git",
+        "subfolder": "hello",
+        "url": "https://github.com/jwestell/hello.git",
+        "revision": "master"
+    }
+    revision_mode = "scm"
 
     def build(self):
         cmake = CMake(self)
