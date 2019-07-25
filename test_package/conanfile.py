44d5ba0f6f38d74e18ba760dde16774a4ad90153
Jamie Westell <jwestell@gmail.com>
2019-07-24 21:26:56 -0700

    Updated test_package to use cmake_find_package generator

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -3,19 +3,13 @@ from conans import ConanFile, CMake, tools
 
 class TmpTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake_find_package"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
-
     def test(self):
         if not tools.cross_building(self.settings):
-            os.chdir("bin")
             self.run(".%stest-hello" % os.sep)
